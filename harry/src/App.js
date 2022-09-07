0de4b6fbd43eca96af036f157c1b9cf8678eca63
priyanka <priyankaphulmante98@gmail.com>
2022-09-07 16:03:31 +0530

    lecture-14

--- harry/src/App.js
+++ harry/src/App.js
@@ -2,14 +2,49 @@ import './App.css';
 import About from './Components/About';
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
+import React, {useState} from 'react'
+import Alert from './Components/Alert';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] =useState(null)
+
+
+
+  const showAlert = (message, type) => {
+ setAlert({
+  msg: message,
+  type :type,
+ })
+
+ setTimeout(() => {
+     setAlert(null);
+ },2000);
+
+  }
+
+
+  const toggleMode =()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor ="black";
+      showAlert("Dark mode has been enabled",'sucess');
+
+    }else{
+    setMode('light');
+    document.body.style.backgroundColor ="white";
+    showAlert('light mode has been enabled', 'sucess');
+    }
+  }
+
   return (
      <>
     {/* <Navbar /> */}
-     <Navbar title="texttuals"  />
-     <TextForm heading= "enter the text to analyse below"/>
- {/* <About /> */}
+     <Navbar title="texttuals" mode={mode} t0ggleMdoe={toggleMode} />
+   <Alert alert={alert} />
+     <TextForm showAlert={showAlert} heading= "enter the text to analyse below"/>
+ <About />
  </>
   );
 }
--- harry/src/Components/TextForm.js
+++ harry/src/Components/TextForm.js
@@ -9,6 +9,7 @@ export default function TextForm(props) {
         console.log("uppercase was clicked" +  text);
         let newText = text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to uppercase!" , "success");
         // setText("You have clicked to handleUpClick")
     }
 
@@ -16,16 +17,20 @@ export default function TextForm(props) {
         console.log("lowercase was clicked" +  text);
         let hello = text.toLowerCase();
         setText(hello);
+        props.showAlert("Converted to lowercase!" , "success");
     }
 
     const handleClearClick = ()=>{
         let hi = " ";
         setText(hi);
+        props.showAlert("Ctext cleared!" , "success");
+       
     }
 
     const handleOnChange = (event) => {
         // console.log("on change")
         setText(event.target.value);
+       
         // setText("newText"); //correct way to change the state
     }
 
@@ -33,22 +38,23 @@ export default function TextForm(props) {
         var text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
-
+        props.showAlert("Copied to clipboard!" , "success");
     }
 
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
         setText(newText.join(" "));
+        props.showAlert("Romve extra spaces!" , "success");
     }
   
     
     const [text, setText] = useState();
     return (
     <>
-    <div className="container">
+    <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
     <h1>{props.heading}</h1>
    <div className="mb-3">
-  <textarea className="form-control"id="myBox" rows="8" 
+  <textarea className="form-control"  style={{backgroundColor:props.mode==='light'?'white':'gray', color:props.mode==='dark'?'white':'black'}} id="myBox" rows="8" 
      value={text} onChange={handleOnChange}></textarea>
    </div>
   <button className="btn btn primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
@@ -57,14 +63,16 @@ export default function TextForm(props) {
    <button className="btn btn primary mx-2" onClick={handleCopy}>Copy Text</button>
     <button className="btn btn primary mx-2" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
-    
-    <div className="container">
+
+    {/* <div className="container" style={{backgroundColor:props.mode==='dark'?'white':'black'}}>
         <h1>Your text summary</h1>
         <b><p>{text.split(" ").length} words and {text.length} characters</p></b>
         <b><p>{0.008 * text.split(" ").length}Minutes read</p></b>
         <h2>Prview</h2>
-        <p>{text}</p>
-    </div>
+        <p>{text.length>0 ? text : 'Enter somthing to textbox above to prview it here'}</p>
+    </div> */}
     </>
   )
 }
+
+
